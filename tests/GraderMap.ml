60934d388842159338c54e800061fa33a925e515
sonyasimk <sonya.simkin@gmail.com>
2026-03-09 16:27:32 -0400

    suppress nonexhaustive match in gradermap

--- tests/GraderMap.ml
+++ tests/GraderMap.ml
@@ -4,6 +4,4 @@ let get problem task =
   match problem with
     "sum" -> 
       (match task with
-        "sum" -> (module SumGrader.Grade : GRADER)
-      | _ -> failwith (Printf.sprintf "Task not defined: %s" task))
-  | _ -> failwith (Printf.sprintf "Problem not defined: %s" problem)
+        "sum" -> (module SumGrader.Grade : GRADER))
